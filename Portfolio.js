0ec2afea388b626f2f66492fb28f9e3cad6bb5c7
Dreadknght <157273542+Dreadknght@users.noreply.github.com>
2024-02-07 22:09:35 +0800

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,3 +1,9 @@
-function attendance(){
+function resume(){
+       window.open("https://dreadknght.github.io/rodeinopena.github.io/Resume - Opeña","_blank");
+}
+function das(){
        window.open("https://dreadknght.github.io/rodeinopena.github.io/DAS-31G6_2023-2024.pdf","_blank");
+}
+function accomplishment(){
+       window.open("https://dreadknght.github.io/rodeinopena.github.io/Accomplishment Report - Opeña.pdf","_blank");
 }
