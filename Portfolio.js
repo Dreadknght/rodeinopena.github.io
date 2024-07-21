86847cc3763db834c47cd981932793007cd59150
Dreadknght <157273542+Dreadknght@users.noreply.github.com>
2024-07-21 16:42:02 +0800

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,9 +1,9 @@
 function das(){
-       window.open("https://docs.google.com/spreadsheets/d/1CkhNdOJkDDXPp9EWHVg33puKDy8B5VibnE7zEeKRuSA/edit?fbclid=IwZXh0bgNhZW0CMTAAAR2nJ9EXHuKOOocivrIrv_813IoESyKWG_w8G2H4daaKnsDZ4X_BwWZUp_w_aem_WGQhCuJiWZw-StBt8DYMCg&gid=947580647#gid=947580647","_blank");
+       window.open("https://docs.google.com/spreadsheets/d/1CkhNdOJkDDXPp9EWHVg33puKDy8B5VibnE7zEeKRuSA/edit?fbclid=IwZXh0bgNhZW0CMTAAAR2nJ9EXHuKOOocivrIrv_813IoESyKWG_w8G2H4daaKnsDZ4X_BwWZUp_w_aem_WGQhCuJiWZw-StBt8DYMCg&gid=947580647#gid=947580647","_self");
 }
 function resume(){
-       window.open("https://drive.google.com/drive/my-drive","_blank");
+       window.open("https://drive.google.com/drive/my-drive","_self");
 }
 function accomplishment(){
-       window.open("https://drive.google.com/drive/my-drive","_blank");
+       window.open("https://drive.google.com/drive/my-drive","_self");
 }
